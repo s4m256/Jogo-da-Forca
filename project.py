bf8a9ad08cb749e9f4b4bddf58210b550ec540c2
s4m256 <al.2312.samuelsantiago@gmail.com>
2025-04-12 13:43:19 -0300

    Delete project.py

--- project.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image
-
-with Image.open("C:/Users/User/Desktop/imagem_base.jpg") as imagem:
-    print(f"Formato da imagem: {imagem.format}")
-    print(f"Dimensões da imagem: {imagem.size}")
-    print(f"Modo da imagem: {imagem.mode}")
-    imagem.show()
-
-    
